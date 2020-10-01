1754e9add210a0db8a3a79945628eba2a5702a72
TriumfThye Hansen <triumfthye@hotmail.com>
2020-10-01 13:27:43 +0200

    Step5 and & 6.

---
+++ Data/PersonSequencer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace TodoApp.Data
+{
+    public class PersonSequencer
+    {
+        static int personId;
+
+        public static int NextPersonId()
+        {
+            return ++personId;
+        }
+
+        public static void Reset()
+        {
+            personId = 0;
+        }
+
+    }//Class
+}//Namespace
