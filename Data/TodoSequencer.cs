75fdb85b0b81b6f55d62acd9e1617eba40f98639
TriumfThye Hansen <triumfthye@hotmail.com>
2020-10-01 17:23:00 +0200

    Step7.

---
+++ Data/TodoSequencer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace TodoApp.Data
+{
+    public class TodoSequencer
+    {
+        static int todoId;
+
+        public static int NextTodoId()
+        {
+            return ++todoId;
+        }
+
+        public static void Reset()
+        {
+            todoId = 0;
+        }
+
+    }//Class
+}//Namespace
